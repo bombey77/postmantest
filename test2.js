a1d48e3448ce738bc65e1ed5bac2a395f7dc1d56
bombey77 <bombey77@mail.ru>
2019-02-04 16:52:39 +0200

    Added test2.js

---
+++ test2.js
@@ -0,0 +1,22 @@
+var fs = require('fs');
+
+fs.open('mynewfile.csv', 'w', function (err, file) {
+  if (err) throw err;
+  console.log('Saved!');
+});
+
+var content2 = fs.readFileSync("environment4.json");
+var json2 = JSON.parse(content2);
+var array = json2.values[9].value;
+var array2 = array.split(",");
+
+array2.forEach(function(element) {
+fs.appendFile('mynewfile.csv', element + '\n', function (err) {
+  if (err) throw err;
+  console.log('Updated!');
+});
+});
+
+
+
+
